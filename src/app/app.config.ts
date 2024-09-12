78c57abad0e629f238bd9a75397da4de9ca09a47
Valentin Gradia <valentingonzalezgradia@gmail.com>
2024-09-11 22:07:01 -0300

    Sprint 1 hecho

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"salajuegos-743e4","appId":"1:18275479696:web:a27e25904b5bf172c2e815","storageBucket":"salajuegos-743e4.appspot.com","apiKey":"AIzaSyAtPuoFhgUktA4MO8TX3cLvLg9zfUFcBLA","authDomain":"salajuegos-743e4.firebaseapp.com","messagingSenderId":"18275479696","measurementId":"G-VL2C40L6PR"})), provideFirestore(() => getFirestore())]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production : false,
+    firebaseConfig : {
+        apiKey: "AIzaSyAtPuoFhgUktA4MO8TX3cLvLg9zfUFcBLA",
+        authDomain: "salajuegos-743e4.firebaseapp.com",
+        projectId: "salajuegos-743e4",
+        storageBucket: "salajuegos-743e4.appspot.com",
+        messagingSenderId: "18275479696",
+        appId: "1:18275479696:web:a27e25904b5bf172c2e815",
+        measurementId: "G-VL2C40L6PR"
+    }
+}

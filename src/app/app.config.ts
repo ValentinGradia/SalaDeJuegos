9464794fafbea43fcf3f4779062ec8475fe9a42f
Valentin Gradia <valentingonzalezgradia@gmail.com>
2024-09-16 17:56:20 -0300

    Sincronizacion con firebase auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,27 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"salajuegos-743e4","appId":"1:18275479696:web:a27e25904b5bf172c2e815","storageBucket":"salajuegos-743e4.appspot.com","apiKey":"AIzaSyAtPuoFhgUktA4MO8TX3cLvLg9zfUFcBLA","authDomain":"salajuegos-743e4.firebaseapp.com","messagingSenderId":"18275479696","measurementId":"G-VL2C40L6PR"})), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'salajuegos-743e4',
+        appId: '1:18275479696:web:a27e25904b5bf172c2e815',
+        storageBucket: 'salajuegos-743e4.appspot.com',
+        apiKey: 'AIzaSyAtPuoFhgUktA4MO8TX3cLvLg9zfUFcBLA',
+        authDomain: 'salajuegos-743e4.firebaseapp.com',
+        messagingSenderId: '18275479696',
+        measurementId: 'G-VL2C40L6PR',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
 };
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,33 +1,71 @@
-import { Component, AfterViewInit, ElementRef, ViewChild, Input  } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild, Input,Inject  } from '@angular/core';
 import { AppComponent } from "../../app.component";
 import Swal from 'sweetalert2';
 import { RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { error } from 'console';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [AppComponent, RouterModule],
+  imports: [AppComponent, RouterModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  correo !: string;
+  contrasenia !: string;
   @ViewChild('container') container!: ElementRef;
   @ViewChild('register') registerBtn!: ElementRef;
   @ViewChild('login') loginBtn!: ElementRef;
   @ViewChild('loginToggle') loginToggleBtn!: ElementRef;
   @ViewChild('registerToggle') registerToggleBtn!: ElementRef
 
-  constructor(private router : Router){}
+  constructor(private router : Router, private auth : Auth){}
 
-  accederAplicacion(msg : string)
+
+  accederAplicacion()
   {
-    Swal.fire({
-      icon: "success",
-      title: "Usuario "+msg+" con exito",
-      showConfirmButton: false,
-      timer: 1500
+    createUserWithEmailAndPassword(this.auth, this.correo, this.contrasenia)
+    .then((userCredential) => {
+      Swal.fire({
+        icon: "success",
+        title: "Usuario registrado con exito",
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.router.navigate(['/home']);
+    })
+    .catch((error) => {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.message
+      });
     });
-    this.router.navigate(['/home']);
+  }
+
+  accederAplicacionLogin()
+  {
+    signInWithEmailAndPassword(this.auth,this.correo,this.contrasenia)
+    .then((user) => {
+      Swal.fire({
+        icon: "success",
+        title: "Usuario logeado con exito",
+        showConfirmButton: false,
+        timer: 1500
+      });
+      console.log(user);
+      this.router.navigate(['/home']);
+    })
+    .catch((error) => {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.message
+      });
+    });
   }
 
   ngAfterViewInit() {
